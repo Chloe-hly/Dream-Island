e8814f8159809b22e9b4d2595f802cc0c658d63a
Chloe-hly <helychloe2801@gmail.com>
2025-11-30 19:11:53 +0100

    Rename Interface class to Principale and update methods

--- Projet/codes/interface.py
+++ Projet/codes/interface.py
@@ -1,2 +1,49 @@
-class Interface:
+class Principale:
+    
+    def __init__(self, menu_gauche, barre_indicateur, notifications, map, menu_actif):
+        self.menu_gauche
+        self.barre_indicateur
+        self.notifications
+        self.map
+        self.menu_actif
+        
+    def draw(self):
+        
+    def update(self):
+    
+    def ouverture_menu(self):
+    
+    def affiche_notification(self):
+        
+    def update(self):
+    
+        
+class MenuGauche:
+    
+    def __init__(self, liste_boutons, menu_actif, notif_non_lues):
+        self.liste_boutons
+        self.menu_actif
+        self.notif_non_lues
+    
+    def draw(self):
+        
+    def clic(self, x, y):
+        
+    def devenir_actif(self):
+    
+    def afficher_point(self):
+        
+
+class Menu:
+    
+    def __init__(self):
+        
+
+class HUD:
+    
+    def __init__(self):
+        
+        
+class
+
     
