b664093238237e4cc66832175687d69232cf54da
Chloe-hly <helychloe2801@gmail.com>
2026-02-09 14:13:06 +0100

    Update indicateurs.py

--- code/indicateurs.py
+++ code/indicateurs.py
@@ -1,43 +1,12 @@
-''' indicateurs principaux :
-
-argent
-
-bonheur
-
-population
-
-energie
-
-nourriture
-
-eau'''
-
 class Indicateurs:
-    
     def __init__(self):
-        
         self.valeurs = {
-            "argent": 1000,
+            "argent": 10000,
+            "pollution": 0,
+            "biodiversite": 50,
             "bonheur": 50,
-            "population": 10,
-            "energie": 20,
-            "nourriture": 20,
-            "eau": 20,
+            "population": 0
         }
-        
-        
-        self.variation = {
-            "argent": 0,
-            "bonheur": 0,
-            "population": 0,
-            "energie": 0,
-            "nourriture": 0,
-            "eau": 0,
-        }
-
-    def appliquer_variation(self):
-        for cle in self.valeurs:
-            self.valeurs[cle] += self.variation[cle]
 
     def modifier(self, cle, valeur):
         if cle in self.valeurs:
@@ -45,3 +14,4 @@ class Indicateurs:
 
     def __str__(self):
         return str(self.valeurs)
+
